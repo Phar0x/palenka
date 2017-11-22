d256bfb033ba14f69ffe26fd62c7d2b993afadc7
Matej Marek <matej.marek94@gmail.com>
2017-11-22 22:45:01 +0100

    vypis

--- src/main/java/sk/palenka/main/Main.java
+++ src/main/java/sk/palenka/main/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Game g = new Game("Title", 500, 400);
         g.start();
+        System.out.println("fuck off");
     }
 
 }
